3912132c420a4c1e3841f1eedef6fc1867531d10
Вячеслав Буглов <74040552+vbuglov@users.noreply.github.com>
2021-07-05 17:32:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import {} from 'ramda'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Edit to see some magic happen!</h2>
+    </div>
+  );
+}
